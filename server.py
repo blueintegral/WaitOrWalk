29958252c7a0a032b65cdc626e4dd067edf7d744
Tanner Smith <tanner@tssoftware.net>
2013-03-27 12:23:54 -0400

    Remove parentheses from conditionals.

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def weather():
 	global cold
 	global rain
 
-	if (time.time() - lastWeatherCheckTime > 300):
+	if time.time() - lastWeatherCheckTime > 300:
 		lastWeatherCheckTime = time.time() # Reset timer
 		urlResult = urllib2.urlopen('http://api.wunderground.com/api/your-api-key-here/conditions/q/30332.json')
 		rawJSON = urlResult.read()
@@ -39,17 +39,17 @@ def weather():
 
 		temperature = parsedJSON['current_observation']['temp_f']
 
-		if (temperature < 60):
+		if temperature < 60:
 			cold = True # It's cold
 
 		conditions = parsedJSON['current_observation']['weather']
 
-		if (conditions == 'Light Drizzle' or conditions == 'Heavy Drizzle' or conditions == 'Light Rain' or conditions == 'Heavy Rain' or conditions == 'Light Hail' or conditions == 'Heavy Hail' or conditions == 'Light Rain Mist' or conditions == 'Heavy Rain Mist' or conditions == 'Light Rain Showers' or conditions == 'Heavy Rain Showers' or conditions == 'Light Hail Showers' or conditions == 'Heavy Hail Showers' or conditions == 'Light Small Hail Showers' or conditions == 'Heavy Small Hail Showers' or conditions == 'Light Thunderstorm' or conditions == 'Heavy Thunderstorm' or conditions == 'Light Thunderstorms and Rain' or conditions == 'Heavy Thunderstorms and Rain' or conditions == 'Light Thunderstorms with Hail' or conditions == 'Heavy Thunderstorms with Hail' or conditions == 'Light Thunderstorms with Small Hail' or conditions == 'Heavy Thunderstorms with Small Hail' or conditions == 'Light Freezing Drizzle' or conditions == 'Heavy Freezing Drizzle' or conditions == 'Light Freezing Rain' or conditions == 'Heavy Freezing Rain' or conditions == 'Small Hail'):
+		if conditions == 'Light Drizzle' or conditions == 'Heavy Drizzle' or conditions == 'Light Rain' or conditions == 'Heavy Rain' or conditions == 'Light Hail' or conditions == 'Heavy Hail' or conditions == 'Light Rain Mist' or conditions == 'Heavy Rain Mist' or conditions == 'Light Rain Showers' or conditions == 'Heavy Rain Showers' or conditions == 'Light Hail Showers' or conditions == 'Heavy Hail Showers' or conditions == 'Light Small Hail Showers' or conditions == 'Heavy Small Hail Showers' or conditions == 'Light Thunderstorm' or conditions == 'Heavy Thunderstorm' or conditions == 'Light Thunderstorms and Rain' or conditions == 'Heavy Thunderstorms and Rain' or conditions == 'Light Thunderstorms with Hail' or conditions == 'Heavy Thunderstorms with Hail' or conditions == 'Light Thunderstorms with Small Hail' or conditions == 'Heavy Thunderstorms with Small Hail' or conditions == 'Light Freezing Drizzle' or conditions == 'Heavy Freezing Drizzle' or conditions == 'Light Freezing Rain' or conditions == 'Heavy Freezing Rain' or conditions == 'Small Hail':
 			rain = True # It's raining
 
-	if (rain):
+	if rain:
 		return str(1)
-	elif (cold):
+	elif cold:
 		return str(2)
 	else:
 		return str(0)
@@ -69,87 +69,87 @@ def start_api():
 			return str(waitResult) + " " + str(busResult)
 
 def getRoute(start, end):
-	if (start == "fitten"):
-		if (end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth"):
+	if start == "fitten":
+		if end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth":
 			# Take the red route  
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "mcm8th"):
-		if (end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth"):
+	if start == "mcm8th":
+		if end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "8thhemp"):
-		if (end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth"):
+	if start == "8thhemp":
+		if end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "fershemrt"):
+	if start == "fershemrt":
 		#This is the one place that doesn't have both a red and blue stop. You have to take red from here.
 		route = "red"
-	if (start == "fersstmrt"):
-		if (end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher"):
+	if start == "fersstmrt":
+		if end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "fersatmrt"):
-		if (end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers"):
+	if start == "fersatmrt":
+		if end == "fersatmrt" or end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "ferschmrt"):
-		if (end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud"):
+	if start == "ferschmrt":
+		if end == "ferschmrt" or end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "5thfowl"):
-		if (end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud"):
+	if start == "5thfowl":
+		if end == "5thfowl" or end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "tech5th"):
-		if (end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" ):
+	if start == "tech5th":
+		if end == "tech5th" or end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" :
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "tech4th"):
-		if (end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" ):
+	if start == "tech4th":
+		if end == "tech4th" or end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" :
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "techbob"):
-		if (end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" ):
+	if start == "techbob":
+		if end == "techbob" or end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" :
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "technorth"):
-		if (end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp"):
+	if start == "technorth":
+		if end == "technorth" or end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "northeave_a"):
-		if (end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp"):
+	if start == "northeave_a":
+		if end == "norteave_a" or end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "ferstcher"):
-		if (end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt"):
+	if start == "ferstcher":
+		if end == "ferstcher" or end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "hubfers"):
-		if (end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt"):
+	if start == "hubfers":
+		if end == "hubfers" or end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "centrstud"):
-		if (end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt"):
+	if start == "centrstud":
+		if end == "centrstud" or end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt":
 			route = "red"
 		else:
 			route = "blue"
-	if (start == "765femrt"):
-		if (end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt"):
+	if start == "765femrt":
+		if end == "765femrt" or end == "fitten" or end == "mcm8th" or end == "8thhemp" or end == "fershemrt" or end == "fersstmrt" or end == "fersatmrt" or end == "ferschmrt":
 			route = "red"
 		else:
 			route = "blue"
@@ -171,21 +171,21 @@ def getNextBusTime(start, route):
 	soup = BeautifulSoup(response)
 	available = True
 
-	if (not soup):
+	if not soup:
 		available = False
 		result = 10000
 
-	if (soup.findAll(text='No prediction') != []):
+	if soup.findAll(text='No prediction') != []:
 		available = False
 		result = 10000
 
-	if (available):
+	if available:
 		prediction = soup.find('td', {'class':"predictionNumberForFirstPred"})
 		result = []
-		if (prediction and prediction.find('div')):
+		if prediction and prediction.find('div'):
 			result = prediction.find('div').string.split(';')[1].strip()
 
-			if (result == "Arriving"):
+			if result == "Arriving":
 				result = 0
 		else:
 			result = 10000
@@ -208,7 +208,7 @@ def shouldWait(start, end, route):
 	walkTime = get_time(start, end, "walking")
 
 	# Compare two times
-	if (waitTime < walkTime):
+	if waitTime < walkTime:
 		return True
 	else:
 		return False
@@ -219,39 +219,39 @@ def get_time(start, end, method):
 	parsedJSON = json.loads(rawJSON)
 
 	# Figure out destination number
-	if (end == "fitten"):
+	if end == "fitten":
 		end = 0
-	if (end == "mcm8th"):
+	if end == "mcm8th":
 		end = 1
-	if (end == "8thhemp"):
+	if end == "8thhemp":
 		end = 2
-	if (end == "fershemrt"):
+	if end == "fershemrt":
 		end = 3
-	if (end == "fersstmrt"):
+	if end == "fersstmrt":
 		end = 4
-	if (end == "fersatmrt"):
+	if end == "fersatmrt":
 		end = 5
-	if (end == "ferschmrt"):
+	if end == "ferschmrt":
 		end = 6
-	if (end == "5thfowl"):
+	if end == "5thfowl":
 		end = 7
-	if (end == "tech5th"):
+	if end == "tech5th":
 		end = 8
-	if (end == "tech4th"):
+	if end == "tech4th":
 		end = 9
-	if (end == "techbob"):
+	if end == "techbob":
 		end = 10
-	if (end == "technorth"):
+	if end == "technorth":
 		end = 11
-	if (end == "nortavea_a"):
+	if end == "nortavea_a":
 		end = 12
-	if (end == "ferstcher"):
+	if end == "ferstcher":
 		end = 13
-	if (end == "hubfers"):
+	if end == "hubfers":
 		end = 14
-	if (end == "centrstud"):
+	if end == "centrstud":
 		end = 15
-	if (end == "765femrt"):
+	if end == "765femrt":
 		end = 16
 		
 	expectedTime = parsedJSON["rows"][0]["elements"][end]["duration"]["value"]
@@ -260,77 +260,77 @@ def get_time(start, end, method):
 	return expectedTime
 
 def stopsBetween(start, end):
-	if (start == "fitten"):
+	if start == "fitten":
 		start = 1
-	if (start == "mcm8th"):
+	if start == "mcm8th":
 		start = 2
-	if (start == "8thhemp"):
+	if start == "8thhemp":
 		start = 3
-	if (start == "fershemrt"):
+	if start == "fershemrt":
 		start = 4
-	if (start == "fersstmrt"):
+	if start == "fersstmrt":
 		start = 5
-	if (start == "fersatmrt"):
+	if start == "fersatmrt":
 		start = 6
-	if (start == "ferschmrt"):
+	if start == "ferschmrt":
 		start = 7
-	if (start == "5thfowl"):
+	if start == "5thfowl":
 		start = 8
-	if (start == "tech5th"):
+	if start == "tech5th":
 		start = 9
-	if (start == "tech4th"):
+	if start == "tech4th":
 		start = 10
-	if (start == "techbob"):
+	if start == "techbob":
 		start = 11
-	if (start == "technorth"):
+	if start == "technorth":
 		start =12
-	if (start == "nortavea_a"):
+	if start == "nortavea_a":
 		start = 13
-	if (start == "ferstcher"):
+	if start == "ferstcher":
 		start = 14
-	if (start == "hubfers"):
+	if start == "hubfers":
 		start = 15
-	if (start == "centrstud"):
+	if start == "centrstud":
 		start = 16
-	if (start == "765femrt"):
+	if start == "765femrt":
 		start = 17
 	
-	if (end == "fitten"):
+	if end == "fitten":
 		end = 1
-	if (end == "mcm8th"):
+	if end == "mcm8th":
 		end = 2
-	if (end == "8thhemp"):
+	if end == "8thhemp":
 		end = 3
-	if (end == "fershemrt"):
+	if end == "fershemrt":
 		end = 4
-	if (end == "fersstmrt"):
+	if end == "fersstmrt":
 		end = 5
-	if (end == "fersatmrt"):
+	if end == "fersatmrt":
 		end = 6
-	if (end == "ferschmrt"):
+	if end == "ferschmrt":
 		end = 7
-	if (end == "5thfowl"):
+	if end == "5thfowl":
 		end = 8
-	if (end == "tech5th"):
+	if end == "tech5th":
 		end = 9
-	if (end == "tech4th"):
+	if end == "tech4th":
 		end = 10
-	if (end == "techbob"):
+	if end == "techbob":
 		end = 11
-	if (end == "technorth"):
+	if end == "technorth":
 		end =12
-	if (end == "nortavea_a"):
+	if end == "nortavea_a":
 		end = 13
-	if (end == "ferstcher"):
+	if end == "ferstcher":
 		end = 14
-	if (end == "hubfers"):
+	if end == "hubfers":
 		end = 15
-	if (end == "centrstud"):
+	if end == "centrstud":
 		end = 16
-	if (end == "765femrt"):
+	if end == "765femrt":
 		end = 17
 	
-	if (end >= start):
+	if end >= start:
 		stops = end - start - 1
 	else:
 		stops = (17 - start) + end -1
